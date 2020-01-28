88302c96626a46c4fa3dfc420c82a215443a2e78
Nicholas Roberto <njr444@gmail.com>
2020-01-28 12:09:00 -0800

    images with radio buttons are randomly displaying

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 import { products } from './product-data.js';
-import { getRandomProduct } from './utils.js';
+import { checkRandom } from './utils/utils.js';
 
-const newProductArray = products.slice();
+const newProductArray = checkRandom();
+console.log(checkRandom());
+const productBox = document.getElementById('product-container');
+
+const imageOne = document.getElementById('image-one');
+const imageTwo = document.getElementById('image-two');
+const imageThree = document.getElementById('image-three');
+const radioOne = document.getElementById('radio-one');
+const radioTwo = document.getElementById('radio-two');
+const radioThree = document.getElementById('radio-three');
+const labelOne = document.getElementById('label-one');
+const labelTwo = document.getElementById('label-two');
+const labelThree = document.getElementById('label-three');
+
+console.log(newProductArray[0].image);
+imageOne.src = newProductArray[0].image;
+imageTwo.src = newProductArray[1].image;
+imageThree.src = newProductArray[2].image;
+
+radioOne.value = newProductArray[0].id;
+radioTwo.value = newProductArray[1].id;
+radioThree.value = newProductArray[2].id;
+
+// labelOne.textContent = newProductArray[0].name;
+// labelTwo.textContent = newProductArray[1].name;
+// labelThree.textContent = newProductArray[2].name;
 
 
-let clickCounter;
-let userProductChoice = [];
+
+
+//let clickCounter;
+//let userProductChoice = [];
 
 
 
--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,4 @@
-import { products } from "./product-data";
+import { products } from "../product-data.js";
 
     export function getProducts() {
         return products;
@@ -19,24 +19,25 @@ import { products } from "./product-data";
     export function getRandomProduct() {
         const randomProductIndex = Math.floor(Math.random() * products.length);
 
-        return product[randomProductIndex];
+        return products[randomProductIndex];
 
     };
 
     export function checkRandom() {
-    let randomProduct = products.getRandomProduct();
-    let randomProduct2 = products.getRandomProduct();
-    let randomProduct3 = products.getRandomProduct();
+    let randomProduct = getRandomProduct();
+    let randomProduct2 = getRandomProduct();
+    let randomProduct3 = getRandomProduct();
 
     while (randomProduct.id === randomProduct2.id) {
-        randomProduct2 = products.getRandomProduct();
+        randomProduct2 = getRandomProduct();
     }
     while (randomProduct2.id === randomProduct3.id) {
-        randomProduct3 = products.getRandomProduct();
+        randomProduct3 = getRandomProduct();
     }
     while(randomProduct3.id === randomProduct.id) {
-        randomProduct = products.getRandomProduct();
-    }
+        randomProduct = getRandomProduct();
+    } 
+    return [randomProduct, randomProduct2, randomProduct3];
 };
 
     export function findById (id, arr) {
